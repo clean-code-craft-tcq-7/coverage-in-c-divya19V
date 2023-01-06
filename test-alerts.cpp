78bdfb433f93f7c323bf7b74f7aaf5e7fabbf0d2
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2023-01-06 23:45:30 +0530

    Added test cases for battery temperature breach

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,9 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(-10.0, 100.2, 1000.67) == TOO_LOW);
 }
